b981ca784d9137923834bc6edc6bdb20eb82f74f
Eric Brousseau <emb417@gmail.com>
2026-09-12 10:48:07 -0700

    Save local configuration changes before pull

--- certbot/conf/renewal-hooks/post/nginx-reload.sh
+++ certbot/conf/renewal-hooks/post/nginx-reload.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker kill -s HUP nginx
+curl -s -o /dev/null -w "%{http_code}\n" --max-time 10 --unix-socket /var/run/docker.sock -X POST "http://localhost/containers/rpi-docker-compose-nginx-1/kill?signal=HUP"
